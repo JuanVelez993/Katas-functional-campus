21a97d8af14f156347cc05a61d1bcf2620ea83ba
Juan Pablo Velez <juan.velez993@gmail.com>
2022-05-25 19:13:14 -0500

    ADD---kata seven solved

--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -3,12 +3,15 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
+import model.BoxArt;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the id, title, and smallest box art url for every video
@@ -18,7 +21,19 @@ import java.util.Map;
 public class Kata7 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
+        List<Map> kataSeven= movieLists
+                        .stream()
+                        .flatMap(movie -> movie.getVideos().stream())
+                        .map(video->ImmutableMap.of("id",video.getId(),"title",video.getTitle(),"boxArt",
+                                video.getBoxarts()
+                                        .stream()
+                                        .reduce(getSmallest())
+                                        .map(boxArt -> boxArt.getUrl()).get())).collect(Collectors.toList());
+        kataSeven.forEach(System.out::println);
+        return kataSeven;
+    }
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+    private static BinaryOperator<BoxArt> getSmallest() {
+        return (bArt1,bArt2)-> bArt1.getWidth()* bArt1.getHeight() < bArt2.getWidth()* bArt2.getHeight()? bArt1 : bArt2;
     }
 }
